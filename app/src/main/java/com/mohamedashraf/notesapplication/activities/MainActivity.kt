d88d5c03f0163a8702377d9f1993cc744081ec1c
Mohamed-Ashraf1 <mmidoaashraf@gmail.com>
2022-06-09 17:46:59 +0200

    Main activity design

---
+++ app/src/main/java/com/mohamedashraf/notesapplication/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mohamedashraf.notesapplication.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mohamedashraf.notesapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
